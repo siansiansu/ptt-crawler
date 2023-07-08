30fd53a12f5a75b1607b07384ba06b9441cd6728
Alex Su <alexsu@17.media>
2023-07-08 21:06:10 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import csv
 import sys
@@ -12,12 +13,22 @@ end_date = str(sys.argv[2])
 downloadFolder = sys.argv[3]
 keywords = sys.argv[4]
 
+# 從這個頁面往前搜尋
 pageUrl = 'https://www.ptt.cc/bbs/Gossiping/index.html'
 
 Path("./{}".format(downloadFolder)).mkdir(parents=True, exist_ok=True)
 
+now = datetime.now()
+current_time = now.strftime("%Y-%m-%d-%H-%M")
 
-print('文章下載目錄：{}/{}'.format(os.getcwd(), downloadFolder))
+logging.basicConfig(filename='log-' + current_time + '.txt',
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('文章下載目錄：{}/{}'.format(os.getcwd(), downloadFolder))
 
 def get_page(url):
     r = requests.get(url, headers={
@@ -43,9 +54,10 @@ def check_keyword(article):
     return False
 
 def check_date(date):
+    if date == "99-99-99":
+        return True
     return datetime.strptime(date, '%Y-%m-%d') >= datetime.strptime(start_date, '%Y-%m-%d') \
         and datetime.strptime(date, '%Y-%m-%d') <= datetime.strptime(end_date, '%Y-%m-%d')
-
 
 def save_text(date, context, filename):
     month = get_month(date)
@@ -65,41 +77,49 @@ def search_a_page(pageUrl):
     df = pd.read_csv('{}/索引.csv'.format(downloadFolder), encoding='utf-8-sig', index_col=False)
 
     for art in arts:
-        title = art.find('div', class_='title').getText().strip()
+        try:
+            title = art.find('div', class_='title').getText().strip()
+        except:
+            logging.info("[標題不存在] 標題：{}".format(title))
+            title = None
+
         if '本文已被刪除' in title or '板規' in title:
             continue
         try:
             link = 'https://www.ptt.cc' + art.find('div', class_='title').a['href'].strip()
         except:
-            print("[連結不存在] 標題：{}".format(title))
+            logging.info("[連結不存在] 標題：{}".format(title))
             link = None
-        if link:
+        if link and title:
             p = get_page(link)
             try:
                 date = date_format(p.select('#main-content > div:nth-child(4) > span.article-meta-value', limit=1)[0].getText())
             except:
                 date = "99-99-99"
-            title = p.select('#main-content > div:nth-child(3) > span.article-meta-value')[0].getText()
+            # title = p.select('#main-content > div:nth-child(3) > span.article-meta-value')[0].getText()
 
             if not check_date(date):
-                print("[不在輸入的日期範圍內，略過這篇內容] 日期：{}，標題：{}".format(date, title))
+                logging.info("[不在輸入的日期範圍內，略過這篇內容] 日期：{}，標題：{}".format(date, title))
                 return None
 
             article = p.find(id='main-content').getText().strip()
-            author = p.find(class_='article-meta-value').getText().strip()
+            try:
+                author = p.find(class_='article-meta-value').getText().strip()
+            except:
+                author = ""
             if check_keyword(article):
-                print("[含有關鍵字，處理中] 日期：{}，標題：{}".format(date, title))
+                logging.info("[含有關鍵字，處理中] 日期：{}，標題：{}".format(date, title))
                 save_text(date, article, title.replace('/', '_'))
                 df2 = pd.DataFrame([date, title, author, article, link]).T
                 df2.columns = ['日期', '標題', '作者', '內容', '網址']
                 df = pd.concat([df, df2], ignore_index=True, axis=0)
             else:
-                print("[不含有關鍵字，略過這篇內容] 日期：{} ，標題：{}".format(date, title))
+                logging.info("[不含有關鍵字，略過這篇內容] 日期：{} ，標題：{}".format(date, title))
     df.drop_duplicates(subset=['網址'], inplace=True)
     df.to_csv('{}/索引.csv'.format(downloadFolder), encoding='utf-8-sig', index=False)
 
     nextLink = 'https://www.ptt.cc' + rootPage.find("a", string="‹ 上頁")["href"]
-    print("搜尋下一頁：{}".format(nextLink))
+    logging.info("搜尋下一頁：{}".format(nextLink))
     return search_a_page(nextLink)
 
 search_a_page(pageUrl)
